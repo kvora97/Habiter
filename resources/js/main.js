10741740ff043bd067b12cb8521e2dfc94757d40
kvora97 <kenil_vora@hotmail.com>
2017-11-25 00:52:01 -0800

    app completed partly

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+/*
+function buttonClicked() {
+	console.log('Button Clicked');
+}
+*/
+
+// user clicked inside add button
+// if there is any text inside input field, make a new to do item
+document.getElementById('add').addEventListener('click', function() {
+
+	var value = document.getElementById('item').value;
+
+	if (value) {
+		 	}
+
+});
